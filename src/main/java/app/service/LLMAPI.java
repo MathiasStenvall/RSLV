f9d5d2a55d622c973820b42c12f3842ec5484dc1
MathiasStenvall <cph-ms1422@stud.ek.dk>
2026-09-08 15:55:44 +0200

    - fixed logo image on index - added topics to LLM

--- src/main/java/app/service/LLMAPI.java
+++ src/main/java/app/service/LLMAPI.java
@@ -36,7 +36,11 @@ public class LLMAPI {
         Map<String, Object> quiz = Map.of("contents", List.of(
                         Map.of("parts", List.of(
                                         Map.of("text", """
-                                                Generate one multiple-choice question about a fitness topic.
+                                                Generate one multiple-choice question about a one of these fitness topics:
+                                                rir, failure, superset, dropset, set, rep, volume,
+                                                progressive overload, compound, isolation, PR/PB, ROM, volume eating, fasting,
+                                                bulk, cut, neat, deload, recovery, hipertrophy, spot, eccentric, concentric,
+                                                pump, lean.
                                                 
                                                 The question must have exactly four possible answers.
                                                 
